c34f3d156b6f8414c98e6edf6d94ecc6c4d1e40e
Brenda David <brenda.david@acad.pucrs.br>
2022-04-03 18:53:14 -0300

    Query 2 exercício

--- MoviesConsole/Program.cs
+++ MoviesConsole/Program.cs
@@ -28,7 +28,7 @@ namespace MoviesConsoleApp
             //                  p.Movie.Title
             //              };
 
-            var query14 = from p in _db.Characters
+            var query1 = from p in _db.Characters
                                          .Include(per => per.Movie)
                                               .ThenInclude(m => m.Genre)
                                          .Include("Actor")
@@ -43,28 +43,47 @@ namespace MoviesConsoleApp
                           };
 
 
-            foreach (var res in query14)
+            foreach (var res in query1)
             {
 
                 Console.WriteLine("\t {0} \t {1} \t {2} \t {3}", res.Personagem, res.Genero, res.Title, res.Diretor);
             }
 
 
-            var query14b = from e in query14
-                           group e by e.Genero into grp
-                           select new
-                           {
-                               Gen = grp.Key,
-                               Quant = grp.Count()
-                           };
+            Console.WriteLine("\nMostrar o nome e idade de todos atores que já atuaram como James Bond");
 
+            var query2 = from p in _db.Characters
+                                    .Include(per => per.Actor)
+                         where p.CharacterName == "James Bond"
+                         select new 
+                         {
+                             Nome = p.Actor.Name,
+                             IdadeAtores = DateTime.Now.Year - p.Actor.DateBirth.Year
+                         };
 
-            Console.WriteLine("\nTotal por genero...");
-            foreach (var res in query14b)
+            foreach (var res in query2.Distinct())
             {
 
-                Console.WriteLine("\t {0} \t {1}", res.Gen, res.Quant);
+                Console.WriteLine("\t {0} \t {1}", res.Nome, res.IdadeAtores);
             }
+
+
+
+            //var query14b = from e in query14
+            //               group e by e.Genero into grp
+            //               select new
+            //               {
+            //                   Gen = grp.Key,
+            //                   Quant = grp.Count()
+            //               };
+
+
+            //Console.WriteLine("\nTotal por genero...");
+            //foreach (var res in query14b)
+            //{
+
+            //    Console.WriteLine("\t {0} \t {1}", res.Gen, res.Quant);
+            //}
 
 
             Console.ReadKey();
